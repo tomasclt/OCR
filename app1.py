c67a8043790df8d6a22f53841ffeb9868458e546
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-29 14:38:01 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,16 +12,16 @@ img_file_buffer = st.camera_input("Toma una Foto")
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
-      if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
-      else:
-         cv2_img= cv2_img
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
+    
+    if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+    else:
+         cv2_img= cv2_img
     
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
