41744bdbf095a1e44ec8d6abb51b831e1a20ae9f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-29 11:23:21 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -19,8 +19,8 @@ if img_file_buffer is not None:
     # Should output shape: (height, width, channels)
     #st.write(cv2_img.shape)
 
-    #img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    #st.write(pytesseract.image_to_string(img_rgb))
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    st.write(pytesseract.image_to_string(img_rgb))
 
     img_rgb = Image.frombytes('RGB', cv2_img.shape[:2], cv2_img, 'raw', 'BGR', 0, 0)
     text_in=pytesseract.image_to_string(img_rgb)
