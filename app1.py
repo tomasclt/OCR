9ab3b7a9748a4ba735d9feb46f3dd9044b3edbfe
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-29 11:43:55 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,9 +13,9 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    pil_image = Image.open(bytes_data)
-    text = pytesseract.image_to_string(pil_image)
-    st.write(text)
+    #pil_image = Image.open(bytes_data)
+    #text = pytesseract.image_to_string(pil_image)
+    #st.write(text)
 
     # Check the type of cv2_img:
     # Should output: <class 'numpy.ndarray'>
@@ -25,8 +25,8 @@ if img_file_buffer is not None:
     # Should output shape: (height, width, channels)
     #st.write(cv2_img.shape)
 
-    #img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    #st.write(pytesseract.image_to_string(img_rgb))
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    st.write(pytesseract.image_to_string(img_rgb))
 
     #img_rgb = Image.frombytes('RGB', cv2_img.shape[:2], cv2_img, 'raw', 'BGR', 0, 0)
     #text_in=pytesseract.image_to_string(img_rgb)
