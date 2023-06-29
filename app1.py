cac4e71426cf4d95d1b83520e2ed3484c3a2a842
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-29 14:29:46 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -14,12 +14,13 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-   filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+    with st.sidebar:
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
-    if filtro == 'Con Filtro':
-       cv2_img=cv2.bitwise_not(cv2_img)
-    else:
-       cv2_img= cv2_img
+      if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+      else:
+         cv2_img= cv2_img
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text=pytesseract.image_to_string(img_rgb)
