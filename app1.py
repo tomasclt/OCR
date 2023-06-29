2bf4d840adfa85e9fa65b17ef57d2b461caecc6f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-29 14:09:23 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -14,6 +14,8 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
+    
+    cv2_img=v2.bitwise_not(cv2_img)
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text=pytesseract.image_to_string(img_rgb)
     st.write(text) 
